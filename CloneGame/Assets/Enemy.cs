a7fb8d1dd526a22091a1f69713b444ac47bd38df
Tran Minh Son <kakalala2003@gmail.com>
2020-11-07 19:05:13 +0700

    first

---
+++ CloneGame/Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Updtae()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime,Space.World);
+
+    }
+}
+ 
